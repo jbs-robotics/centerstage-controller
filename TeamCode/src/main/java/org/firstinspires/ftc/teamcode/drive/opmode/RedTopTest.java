3d30395be0ed3c38e97d4dab60618dda6d293e56
BobyWoby <98625861+BobyWoby@users.noreply.github.com>
2023-11-05 15:53:07 -0600

    added TFOD switch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RedTopTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RedTopTest.java
@@ -16,18 +16,58 @@ public class RedTopTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-
+        int TFODPrediction = 0;
         waitForStart();
 
         if (isStopRequested()) return;
         drive.setPoseEstimate(new Pose2d(new Vector2d(60, 10), Math.toRadians(180)));
-        Trajectory traj = drive.trajectoryBuilder(new Pose2d())
-                .lineToLinearHeading(new Pose2d(new Vector2d(35, 10), Math.toRadians(-90)))
-                .build();
+        switch(TFODPrediction){
+            case 0: //left
+                Trajectory left1 = drive.trajectoryBuilder(new Pose2d(60, 10, Math.toRadians(180)))
+                        .lineToLinearHeading(new Pose2d(new Vector2d(35, 10), Math.toRadians(-80)))
+                        .build();
+                drive.followTrajectory(left1);
+                //place prop on spike mark
+                Trajectory left2 = drive.trajectoryBuilder(left1.end())
+                        .lineToLinearHeading(new Pose2d(new Vector2d(28, 50), Math.toRadians(180)))
+                        .build();
+                drive.followTrajectory(left2);
+                //place pixel on canvas
+                break;
+            case 1: //center
+                Trajectory center1 = drive.trajectoryBuilder(new Pose2d(60, 10, Math.toRadians(180)))
+                        .forward(27)
+                        .build();
+                drive.followTrajectory(center1);
+                //place prop on spike mark
+                Trajectory center2 = drive.trajectoryBuilder(center1.end())
+                        .lineToLinearHeading(new Pose2d(35.5, 50, Math.toRadians(90)))
+                        .build();
+                drive.followTrajectory(center2);
+                //place pixel on canvas
+                break;
+            case 2: //right
+                Trajectory right1 = drive.trajectoryBuilder(new Pose2d(60, 10, Math.toRadians(180)))
+                        .strafeRight(27)
+                        .build();
+                Trajectory right2 = drive.trajectoryBuilder(right1.end())
+                        .lineToSplineHeading(new Pose2d(30, 32, Math.toRadians(-90)))
+                        .build();
+                drive.followTrajectory(right1);
+                drive.followTrajectory(right2);
+                //place prop on spike mark
+                Trajectory right3 = drive.trajectoryBuilder(right2.end())
+                        .lineToSplineHeading(new Pose2d(42, 50, Math.toRadians(90)))
+                        .build();
+                drive.followTrajectory(right3);
+                //place pixel on canvas
+                break;
+            default:
+                telemetry.addData("wtf how", "no but actually how");
+                break;
+        }
 
-        drive.followTrajectory(traj);
-
-        sleep(2000);
+//        sleep(2000);
 
 //        drive.followTrajectory(
 //                drive.trajectoryBuilder(traj.end(), true)
